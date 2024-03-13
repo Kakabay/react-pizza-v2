b6490bd31a5db12419af1173179dc5549fed3f15
Kakabay <2kakabayashyrberdyew@gmail.com>
2024-03-13 14:01:18 +0500

    categories rendered and made active

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -1,13 +1,32 @@
+import { useState } from "react";
+
 const Categories = () => {
+  const [activeIndex, setActiveIndex] = useState<number>(0);
+
+  const categories: string[] = [
+    "Все",
+    "Мясные",
+    "Вегетарианская",
+    "Гриль",
+    "Острые",
+    "Закрытые",
+  ];
+
+  const onClickCategory = (index: number) => {
+    setActiveIndex(index);
+  };
+
   return (
     <div className="categories">
       <ul>
-        <li className="active">Все</li>
-        <li>Мясные</li>
-        <li>Вегетарианская</li>
-        <li>Гриль</li>
-        <li>Острые</li>
-        <li>Закрытые</li>
+        {categories.map((category, index) => (
+          <li
+            onClick={() => onClickCategory(index)}
+            className={activeIndex === index ? "active" : ""}
+          >
+            {category}
+          </li>
+        ))}
       </ul>
     </div>
   );
